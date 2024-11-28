b453b2d6133a61c2126d8f846d52dc0298c47fe7
nipunapamuditha <nipunapamuditha234@gmail.com>
2024-11-27 22:17:48 -0500

    ecs add

--- Container Method/main.go
+++ Container Method/main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -14,68 +13,41 @@ import (
 
 var mongoClient *mongo.Client
 
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET")
+func enableCros(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Method", "POST, GET")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
 
 func saveIPHandler(w http.ResponseWriter, r *http.Request) {
-	enableCors(&w)
+	enableCros(&w)
 	if r.Method != "POST" {
 		http.Error(w, "Only POST method is allowed!", http.StatusMethodNotAllowed)
 		return
 	}
-
 	var data map[string]interface{}
 	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
-		log.Printf("JSON decode error: %v", err)
 		http.Error(w, "Error parsing the JSON request!", http.StatusBadRequest)
 		return
 	}
-
-	data["time"] = time.Now().Format("2006-01-02 15:04:05")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	data["time"] = currentTime
 	collection := mongoClient.Database("userData").Collection("IPs")
-	_, err = collection.InsertOne(ctx, data)
+	_, err = collection.InsertOne(context.TODO(), data)
 	if err != nil {
-		log.Printf("MongoDB insert error: %v", err)
 		http.Error(w, "Error saving data to MongoDB", http.StatusInternalServerError)
 		return
 	}
 }
-
 func main() {
-	// Get MongoDB URI from environment variable
-	mongoURI := os.Getenv("MONGODB_URI")
-	if mongoURI == "" {
-		mongoURI = "mongodb://database:27017" // Use container service name
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
 	var err error
-	mongoClient, err = mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	mongoClient, err = mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://54.219.52.229:27017"))
 	if err != nil {
-		log.Fatal("MongoDB connection error:", err)
+		log.Fatal(err)
 	}
-
-	// Verify connection
-	err = mongoClient.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal("MongoDB ping error:", err)
-	}
-	log.Println("Connected to MongoDB successfully!")
-
 	fs := http.FileServer(http.Dir("./templates"))
 	http.Handle("/", fs)
 	http.HandleFunc("/api/saveIP", saveIPHandler)
-
-	log.Println("Server starting on :80")
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
